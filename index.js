db2fe2a339dd417f5e9b995c4d4ddc49357db73e
zekesing007 <happy.rekrutmen@gmail.com>
2025-02-22 13:40:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function askForInput() {
         maxLength: "0",
     });
 
-    console.log(chalk.blue.bold("=== Telegram Channel : NT Exhaust (@NTExhaust) ===", "\x1b[36m"));
+    console.log(chalk.blue.bold("=== Telegram Channel : Cryptokom (@cryptokom2) ===", "\x1b[36m"));
 
     rl.question("Enter recipient addresses (comma-separated): ", (addresses) => {
         const recipients = addresses.split(",").map(addr => addr.trim());
